4dc77f01f5494faea8047c9653962e0c06ecd85c
Andy Chan <andy.chan9930@gmail.com>
2021-10-18 16:42:02 -0400

    commit tree 774f57dc0c2e20151d347b3ebd3596656fbea60c

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function moveDodgerLeft() {
     var left = parseInt(leftNumbers, 10);
           
     if ( left > 0 ) {
-      dodger.style.left = `${left - 1}px`;
+      dodger.style.left = `${left - 5}px`;
     }
   }
   
@@ -12,15 +12,21 @@ function moveDodgerLeft() {
     var left = parseInt(leftNumbers, 10);
   
     if ( left < 360) {
-      dodger.style.left = `${left + 1}px`;
+      dodger.style.left = `${left + 5}px`;
     }
   }
         
-  document.addEventListener('keydown', function(e) {
-    if ( e.which === 37 ) {
-      moveDodgerLeft();
+  // document.addEventListener('keydown', function(e) {
+  //   if ( e.which === 37 ) {
+  //     moveDodgerLeft();
+  //   }
+  //   if ( e.which === 39 ) {
+  //     moveDodgerRight();
+  //   }
+  // });
+
+  document.addEventListener('keydown', element => {
+    if (element.key === "ArrowLeft") {
+      moveDodgerLeft()
     }
-    if ( e.which === 39 ) {
-      moveDodgerRight();
-    }
-  });
+  })
